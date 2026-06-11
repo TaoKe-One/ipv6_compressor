481ef4291a4dba8537c58b468d07b8158bc7a23b
TaoKe-One <877136105@qq.com>
2026-06-12 00:22:09 +0800

    Remove deprecated themes import

--- cmd/gui/main.go
+++ cmd/gui/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/themes"
 
 	"github.com/TaoKe-One/ipv6_compressor/internal/gui"
 )
 
 func main() {
 	a := app.New()
-	a.Settings().SetTheme(themes.DefaultTheme())
 
 	w := a.NewWindow("IPv6 Compressor v2.0")
 	w.SetMaster()
